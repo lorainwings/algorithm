13fe7ebc9b30f8744beaab954ac45df4ead7a10a
lorainwings <lorainwings@gmail.com>
2021-01-05 23:40:24 +0800

    add binary-tree level order

--- __test__/tree.test.js
+++ __test__/tree.test.js
@@ -1,4 +1,4 @@
-const { maxDepth, maxDepthCallee } = require('../tree/leetcode')
+const { maxDepth, maxDepthCallee, minDepth, minDepthByArr, levelOrder,levelOrderWhile } = require('../tree/leetcode')
 
 const btree = {
     val: 3,
@@ -65,7 +65,6 @@ const btree1 = {
 }
 
 describe('二叉树的最大深度BFS遍历版', () => {
-    // 测试用例1
     it('btree->3', () => {
         expect(maxDepth(btree)).toEqual(3);
     });
@@ -75,12 +74,47 @@ describe('二叉树的最大深度BFS遍历版', () => {
 });
 
 describe('二叉树的最大深度DFS递归版', () => {
-    // 测试用例1
     it('btree->3', () => {
         expect(maxDepthCallee(btree)).toEqual(3);
     });
     it('btree1->5', () => {
         expect(maxDepthCallee(btree1)).toEqual(5);
+    });
+});
+
+describe('二叉树的最小深度BFS遍历版', () => {
+    it('btree->2', () => {
+        expect(minDepth(btree)).toEqual(2);
+    });
+    it('btree1->3', () => {
+        expect(minDepthByArr(btree1)).toEqual(3);
+    });
+});
+
+describe('二叉树的最小深度二维数组版', () => {
+    it('btree->2', () => {
+        expect(minDepth(btree)).toEqual(2);
+    });
+    it('btree1->3', () => {
+        expect(minDepthByArr(btree1)).toEqual(3);
+    });
+});
+
+describe('二叉树的层序遍历', () => {
+    it('btree->[[3], [9, 20], [15, 7]]', () => {
+        expect(levelOrder(btree)).toEqual([[3], [9, 20], [15, 7]]);
+    });
+    it('btree1->[[1], [2, 3], [4, 5], [6, 7], [8, 9], [10]]', () => {
+        expect(levelOrder(btree1)).toEqual([[1], [2, 3], [4, 5, 6, 7], [8, 9], [10]]);
+    });
+});
+
+describe('二叉树的层序遍历双层while', () => {
+    it('btree->[[3], [9, 20], [15, 7]]', () => {
+        expect(levelOrderWhile(btree)).toEqual([[3], [9, 20], [15, 7]]);
+    });
+    it('btree1->[[1], [2, 3], [4, 5], [6, 7], [8, 9], [10]]', () => {
+        expect(levelOrderWhile(btree1)).toEqual([[1], [2, 3], [4, 5, 6, 7], [8, 9], [10]]);
     });
 });
 
--- tree/leetcode.js
+++ tree/leetcode.js
@@ -1,7 +1,8 @@
 
-//二叉树的最大深度 bfs实现
+//104二叉树的最大深度 bfs实现
 const maxDepth = (root) => {
     let depth = 0;
+    if (!root) return depth;
     const stack = [root];
     while (stack.length) {
         depth++;
@@ -16,7 +17,7 @@ const maxDepth = (root) => {
     return depth;
 }
 
-// 二叉树的最大深度 dfs实现
+// 104二叉树的最大深度 dfs实现
 const maxDepthCallee = (root) => {
     let depth = 0;
     const dfs = (n, d) => {
@@ -29,7 +30,98 @@ const maxDepthCallee = (root) => {
     return depth;
 }
 
+// 111二叉树的最小深度BFS实现(第一种解法)
+const minDepth = (root) => {
+    let depth = 0;
+    if (!root) return depth;
+    const stack = [root];
+    while (stack.length) {
+        depth++;
+        let level = stack.length;
+        while (level > 0) {
+            const n = stack.shift();
+            if (!n.left || !n.right) return depth;
+            if (n.left) { stack.push(n.left); };
+            if (n.right) { stack.push(n.right); };
+            level--;
+        }
+    }
+    return depth;
+}
+
+// 111二叉树的最小深度BFS实现(第二种解法)
+const minDepthByArr = (root) => {
+    let depth = 0;
+    if (!root) return depth;
+    const stack = [[root, 1]]; // 用二维数组, 保存层级
+    while (stack.length) {
+        const [n, level] = stack.shift();
+        if (!n.left || !n.right) return level;
+        if (n.left) { stack.push([n.left, level + 1]); };
+        if (n.right) { stack.push([n.right, level + 1]); };
+    }
+}
+
+// 102二叉树的层序遍历(第一种解法)
+const levelOrder = (root) => {
+    const level = [];
+    if (!root) return level;
+    const stack = [[root, 0]];
+    while (stack.length) {
+        const [n, l] = stack.shift();
+        if (level[l]) level[l].push(n.val);
+        else level[l] = [n.val];
+        if (n.left) stack.push([n.left, l + 1]);
+        if (n.right) stack.push([n.right, l + 1]);
+    }
+    return level;
+}
+
+// 102二叉树的层序遍历(第二种解法)
+const levelOrderWhile = (root) => {
+    const level = [];
+    if (!root) return level;
+    const stack = [root];
+    while (stack.length) {
+        let childLen = stack.length; // 每层的开始都是所有的儿子
+        level.push([]);
+        while (childLen--) { // 该循环内部都是同一层级
+            const n = stack.shift();
+            level[level.length - 1].push(n.val);
+            if (n.left) { stack.push(n.left); };
+            if (n.right) { stack.push(n.right); };
+        }
+    }
+    return level;
+}
+
+const btree = {
+    val: 3,
+    left: {
+        val: 9,
+        left: null,
+        right: null
+    },
+    right: {
+        val: 20,
+        left: {
+            val: 15,
+            left: null,
+            right: null
+        },
+        right: {
+            val: 7,
+            left: null,
+            right: null
+        }
+    }
+}
+
 module.exports = {
     maxDepth,
-    maxDepthCallee
+    maxDepthCallee,
+    minDepth,
+    minDepthByArr,
+    levelOrder,
+    levelOrderWhile
 }
