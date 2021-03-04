e312c1cdf327ac851ce890103770225f8be9cbe5
lorainwings <lorainwings@gmail.com>
2021-03-04 15:27:18 +0800

    feat(add exam): 更新exam

--- recursion/index.js
+++ recursion/index.js
@@ -1,9 +1,27 @@
-const findAllIp = (str) => {
-  for(let i=0;i<3;i++){
-      str[i]
-  }
+const fibonacci = (n) => {
+    if (n <= 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+// 蹦床函数
+const trampoline = (fn) => {
+    let active = false;
+    let value = null;
+    const argsArr = [];
+    return function (...args) {
+        argsArr.push(args);
+        if(!active) {
+            active = true;
+            while (argsArr.length) {
+                value = fn.apply(this, argsArr.shift());
+            };
+            active = false;
+            return value;
+        }
+    }
 }
 
 module.exports = {
-    findAllIp
+    fibonacci,
+    trampoline
 }
